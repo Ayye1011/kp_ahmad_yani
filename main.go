68cb9f13a62408b634503449c670a584a977dd4e
Ayye1011 <tegar4bifal1@gmail.com>
2023-06-01 00:27:14 +0700

    connects main.go with the echo library and provides the Struct User and GET.Users endpoints

--- main.go
+++ main.go
@@ -3,13 +3,36 @@ package main
 import (
 	"fmt"
 
+	"github.com/labstack/echo/v4"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
+type User struct {
+	Id       uint8  `gorm:primaryKey AutoIncrement json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func main() {
+	connectDatabase()
+	e := echo.New()
+	e.GET("/username", GetUsernameController)
+	e.Start(":8000")
+}
+
+func GetUsernameController(c echo.Context) error {
+	var users []User
+	result := DB.Find(&users)
+	if result.Error != nil {
+		return c.JSON(500, nil)
+	}
+	return c.JSON(200, users)
+}
+
+func connectDatabase() {
 	dsn := "root:Sitialbir@tcp(127.0.0.1:1011)/unjuk_keterampilan?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -17,5 +40,4 @@ func main() {
 		panic("Database Error")
 	}
 	fmt.Println("Success")
-
 }
