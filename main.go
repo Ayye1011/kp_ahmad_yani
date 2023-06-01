3d0692b090aeb04caba991601ae21b6e76cb7192
Ayee1011 <tegar4bifal1@gmail.com>
2023-06-01 10:55:22 +0700

    added the AutoMigration func and fixed the LoginRequest in the main() func in the main.go file

--- main.go
+++ main.go
@@ -2,14 +2,57 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
+	"github.com/labstack/echo/v4"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
+type User struct {
+	Id       uint8  `gorm:primaryKey AutoIncrement json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type BaseResponese struct {
+	Message string
+	Data    interface{}
+}
+
 func main() {
+	connectDatabase()
+	e := echo.New()
+	e.GET("/username", GetUsernameController)
+	e.POST("/username", LoginRequest)
+	e.Start(":8000")
+}
+
+func GetUsernameController(c echo.Context) error {
+	var users []User
+	result := DB.Find(&users)
+	if result.Error != nil {
+		return c.JSON(500, nil)
+	}
+	return c.JSON(200, BaseResponese{
+		Message: "Success",
+		Data:    users,
+	})
+}
+
+func LoginRequest(c echo.Context) error {
+	var userInput User
+	c.Bind(&userInput)
+	result := DB.Create(&userInput)
+	if result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, nil)
+	}
+	return c.JSON(http.StatusOK, userInput)
+}
+
+func connectDatabase() {
 	dsn := "root:Sitialbir@tcp(127.0.0.1:1011)/unjuk_keterampilan?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -17,5 +60,8 @@ func main() {
 		panic("Database Error")
 	}
 	fmt.Println("Success")
+}
 
+func migration() {
+	DB.AutoMigrate(&User{})
 }
