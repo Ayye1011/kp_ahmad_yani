9916c40d1abd0ed9586b1e3408f843e95b428837
Ayee1011 <tegar4bifal1@gmail.com>
2023-06-03 16:15:56 +0700

    added the AddPort func

--- configs/config.go
+++ configs/config.go
@@ -1,16 +1,41 @@
 package configs
 
 import (
+	"fmt"
 	"kpahmadyani/models"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("gagal Load")
+	}
+}
+
+type DBConfig struct {
+	Username string
+	Password string
+	Host     string
+	Port     string
+	Name     string
+}
+
 func ConnectDatabase() {
-	dsn := "root:Sitialbir@tcp(127.0.0.1:1011)/unjuk_keterampilan_ok?charset=utf8mb4&parseTime=True&loc=Local"
+	var dbConfig DBConfig = DBConfig{
+		Username: os.Getenv("DB_USERNAME"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Name:     os.Getenv("DB_NAME"),
+	}
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbConfig.Username, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Name)
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
--- main.go
+++ main.go
@@ -3,10 +3,25 @@ package main
 import (
 	"kpahmadyani/configs"
 	"kpahmadyani/routes"
+	"os"
 )
 
-func main() {
+func init() {
+	configs.LoadEnv()
 	configs.ConnectDatabase()
+
+}
+
+func main() {
 	e := routes.Init()
-	e.Start(":8080")
+	e.Start(":" + getPort())
+	// e.Start(":8080")
+}
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	return port
 }
